30c488104ae77bf372e5d507e4e05cad06a9ba6f
IvonneBurgos <irburgos@espol.edu.ec>
2017-07-24 19:43:25 -0500

    Review Screen design using fragments

--- app/src/main/java/com/tripget/tripget/MainActivity.java
+++ app/src/main/java/com/tripget/tripget/MainActivity.java
@@ -1,10 +1,14 @@
 package com.tripget.tripget;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -31,7 +35,7 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener, ReviewTripFragment.OnFragmentInteractionListener {
 
 
     private GoogleApiClient googleApiClient;
@@ -47,8 +51,15 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                ReviewTripFragment fragment = new ReviewTripFragment();
+                transaction.add(R.id.main_content, fragment);
+                transaction.commit();
+
+
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
 
@@ -190,6 +201,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
